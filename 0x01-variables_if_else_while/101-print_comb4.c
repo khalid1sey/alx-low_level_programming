7014000d3949be7c88a62a767d515b7f2469ad8b
khalid1sey <lilahseyfu@gmail.com>
2023-10-27 16:13:07 +0300

    done

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - a program that checks wether a i is positive or negative
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	int i, j, k;
+
+
+	for (i = 0; i <= 8; i++)
+	{
+		for (j = i + 1; j <= 9; j++)
+		{
+            for (k = j + 1; k <= 9; k++)
+            {
+			    putchar(i + '0');
+			    putchar(j + '0');
+                putchar(k + '0');
+
+			    if (k != 8 || j != 8 || i != 8)
+			    {
+				    putchar(',');
+				    putchar(' ');
+			    }
+            }
+		}
+	}
+	putchar('\n');
+	return (0);
+}
