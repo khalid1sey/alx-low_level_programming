cca908a8cca018ca717237747a2be6b9d467fec4
khalid1sey <lilahseyfu@gmail.com>
2023-10-27 11:06:53 +0300

    done task 5

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <time.h>
+
+/**
+ * main - a program that checks wether a number is positive or negative
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i != 'e' && i != 'q')
+		{
+			putchar(i);
+		}
+	}
+	printf("\n");
+	return (0);
+}
--- 0x01-variables_if_else_while/c.c
+++ 0x01-variables_if_else_while/c.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
-#include <time.h>
-#include <stdlib.h>
 
-int main(void){
-    int n, last_digit;  
+int main() {
+    char letter;
 
-  return 0;
+    for (letter = 'a'; letter <= 'z'; letter++) {
+        if (letter != 'h' && letter != 'l') {
+            putchar(letter);
+        }
+    }
+
+    putchar('\n');
+    return 0;
 }
