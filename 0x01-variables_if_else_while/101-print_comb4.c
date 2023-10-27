f36b27ed761bb36b21e4f7181e66121e30cead78
khalid1sey <lilahseyfu@gmail.com>
2023-10-27 16:15:56 +0300

    fixed errors

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -14,18 +14,18 @@ int main(void)
 	{
 		for (j = i + 1; j <= 9; j++)
 		{
-            for (k = j + 1; k <= 9; k++)
-            {
-			    putchar(i + '0');
-			    putchar(j + '0');
-                putchar(k + '0');
+			for (k = j + 1; k <= 9; k++)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(k + '0');
 
-			    if (k != 8 || j != 8 || i != 8)
-			    {
-				    putchar(',');
-				    putchar(' ');
-			    }
-            }
+				if (k != 8 || j != 8 || i != 8)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
 		}
 	}
 	putchar('\n');
