9d670c4a8f0d9a2187c80f3520ab554c0b3ca527
khalid1sey <lilahseyfu@gmail.com>
2023-10-31 22:18:15 +0300

    done t5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,18 +10,18 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		putchar('+');
 		return (1);
+		putchar('+');
 	}
 	else if (n == 0)
 	{
-		putchar('0');
 		return (0);
+		putchar('0');
 	}
 	else if (n < 0)
 	{
-		putchar('-');
 		return (-1);
+		putchar('-');
 	}
 
 	return (0);
