d7d4f5b0230f0d17d188dc92d949dfc57b5f4ff4
khalid1sey <lilahseyfu@gmail.com>
2023-10-31 21:57:04 +0300

    done task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,23 +5,14 @@
  * _isalpha - a function that checks lowercase character.
  *
  *@c: given character
- * Return: 1 (Success) | 0 
+ * Return: 1 (Success) | 0
 */
 int _isalpha(int c)
 {
-    
+	if (c <= 122 && c >= 65)
+		return (1);
+	else
+		return (0);
 
-    int i;
-    int temp = c;
-
-    for (i = 65; i <= 122; i++)
-    {
-        if (temp <= 65 && temp <= 122)
-        {
-            return (1);
-        } else{
-            return (0);
-        }
-    }
-    return (0);
+	return (0);
 }
