1605b50900136bb499f5b4761bdbb1994330800e
khalid1sey <lilahseyfu@gmail.com>
2023-11-02 12:12:15 +0300

    done 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_square - a function that draws a diagonal line on the terminal.
+ * @size: distance
+ * Return: 0
+*/
+void print_square(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+		putchar('\n');
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			putchar('#');
+		}
+		putchar('\n');
+	}
+	putchar('\n');
+}
