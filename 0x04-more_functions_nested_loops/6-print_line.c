b935d1939390bb899f1088d79e60ba75a3388e75
khalid1sey <lilahseyfu@gmail.com>
2023-11-02 14:15:12 +0300

    done

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -14,7 +14,7 @@ void print_line(int n)
 
 	for (i = 0; i < n; i++)
 	{
-		putchar('_' + 0);
+		putchar('_');
 	}
 	putchar('\n');
 
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
