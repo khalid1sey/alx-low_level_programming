8d1f3e7c62bf5d0072814c40370fad54b838515e
khalid1sey <lilahseyfu@gmail.com>
2023-11-09 19:50:01 +0300

    done t2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,25 +1,25 @@
 #include "main.h"
 
 /**
- * strcpy - a custom function to implement builtin strcpy
+ * _strncpy - a custom function to implement builtin strcpy
  * @src: source os string
  * @dest: destination
  * @n: bytes read
- * 
+ *
  * Return: copy of given string
 */
 char *_strncpy(char *dest, char *src, int n)
 {
 
 	int i;
-	
+
 	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
 	}
 	for (; i < n; i++)
 	{
-		dest = '\0'; 
+		dest = '\0';
 	}
 
 	return (dest);
