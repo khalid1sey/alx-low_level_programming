3a04b4d84396c0cf54d7cbfea77703d0964ffe45
khalid1sey <lilahseyfu@gmail.com>
2023-11-09 19:47:26 +0300

    done t2

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * leet - a function that encodes a string using rot13
+ * @str: string
+ *
+ * Return: encodded string
+*/
+char *leet(char *str)
+{
+    arr1[] = {a, A, e, E, o, O, t, T, l, L};
+    arr2[] = {4,3,0,7,1}
+
+    for (i )
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -14,6 +14,7 @@ int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *str);
+char *leet(char *str);
 
 int _strlen(char *s);
 
